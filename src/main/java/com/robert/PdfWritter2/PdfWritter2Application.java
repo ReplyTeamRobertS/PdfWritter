77dae8da7f36ba42df482aad09a1b5daa0323246
Robert S <replyteam.roberts@gmail.com>
2022-09-06 12:10:26 +0300

    test

---
+++ src/main/java/com/robert/PdfWritter2/PdfWritter2Application.java
@@ -0,0 +1,14 @@
+package com.robert.PdfWritter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfWritter2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PdfWritter2Application.class, args);
+	}
+
+}
